ef4de9c09f92f881cf13498b4b75a6417032fbff
StephanieBeaton <stefanybeaton@gmail.com>
2014-10-09 14:31:50 -0700

    Solved homework #5

--- lib/class2/exercise5.rb
+++ lib/class2/exercise5.rb
@@ -9,3 +9,9 @@
 #   What's your favorite number?
 #   42
 #   That's ok, I guess, but isn't 43 just a bit better?
+
+puts "What's your favorite number?"
+fav_number = gets.chomp
+better_number = fav_number.to_i + 1
+puts "That's ok, I guess, but isn't " + better_number.to_s +
+     ' just a bit better?'
